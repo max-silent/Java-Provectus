87d9c2e86dbd59e2052dcc7b421ca118ebf3d10e
Max <mirotvoretz@gmail.com>
2017-02-06 20:44:22 +0200

    Test

---
+++ Homework/4d/Practice4d_1/src/main/java/CTO.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Max on 11.01.2017.
+ */
+public class CTO extends IT {
+
+    CTO(String name, String surname, int salary) {
+        super(name, surname, salary);
+    }
+}
---
+++ Homework/4d/Practice4d_1/src/main/java/OfficeManager.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Max on 11.01.2017.
+ */
+public class OfficeManager extends Clerk {
+
+    OfficeManager(String name, String surname, int salary) {
+        super(name, surname, salary);
+    }
+}
